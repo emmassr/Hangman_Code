78c517e0af00a3fd7f08863a328ee12ea8087ce5
emmassr <emma.ssr@gmail.com>
2019-07-18 17:59:10 +0100

    First commit

---
+++ lib/hangman.rb
@@ -0,0 +1,51 @@
+class Hangman
+
+  def check_word(letter, word)
+    word.include?(letter)
+  end
+
+  def is_complete?(word)
+    !word.include?("_")
+  end
+
+  def guess_word(attempts_remaining, word, letter)
+    while attempts_remaining > 0 do
+      check_word(letter, word) # check if true dont remove attempts
+      if check_word(letter, word)
+        attempts_remaining = attempts_remaining
+      else
+        attempts_remaining -= 1
+      end
+    end
+    attempts_remaining
+  end
+    #compare guess_word and check_word and update
+  def show_letter(computer_word, guessed_word, correct_letter)
+    chars = computer_word.split('')
+    hello = chars.each_with_index { |character, index|
+    if character == correct_letter
+        guessed_word.slice!(index)
+        guessed_word.insert(index, correct_letter)
+    end
+  }
+  guessed_word
+  end
+end
+
+
+class RandomWordSelector
+
+  def select_word(words)
+    words.sample() #sample is used for arrays
+  end
+
+end
+
+class WordFileReader #created this classs to use a text file with a list of words for the game, text file named word.txt in Hangman folder
+  def read_file(path_to_file)
+    file = File.open(path_to_file)# this opens the text file
+    file_data = file.readlines.map(&:chomp)#the readlines.map command goes through each word in the list and puts it in an array and the chomp removes the feature of a new line start and places the words next to each other in an array eg: ["word1, "word2"]
+    file.close # closes file
+    file_data  # returns all the words which in is now in the array
+  end
+end
---
+++ spec/hangman_spec.rb
@@ -0,0 +1,100 @@
+require 'hangman'
+describe RandomWordSelector do
+  it "chooses word from single element array randomly" do
+    words = ["AH"]
+    random_word_selector = RandomWordSelector.new()
+    expect(random_word_selector.select_word(words)).to eq("AH")
+  end
+
+  it "chooses word from multiple element array randomly" do
+    words = ["hello", "there"]
+    random_word_selector = RandomWordSelector.new()
+    result = random_word_selector.select_word(words)
+    expect([result].length).to eq(1)
+    expect(words.include?(result)).to eq(true)
+  end
+
+end
+
+describe Hangman do
+  it "is valid if letter is in the word" do
+    letter = "a"
+    word = "apple"
+    hangman = Hangman.new()
+    expect(hangman.check_word(letter, word)).to eq(true)
+  end
+
+  it "is invalid if letter is not in the word" do
+    letter = "z"
+    word = "apple"
+    hangman = Hangman.new()
+    expect(hangman.check_word(letter, word)).to eq(false)
+  end
+
+  it "is valid if the word is complete" do
+    word = "banana"
+    hangman = Hangman.new()
+    expect(hangman.is_complete?(word)).to eq(true)
+  end
+
+  it "is invalid if the word is not complete" do
+    word = "b_nan_"
+    hangman = Hangman.new()
+
+    expect(hangman.is_complete?(word)).to eq(false)
+  end
+
+  it "gives user only one more attempt when guess is wrong " do
+    letter = "z"
+    word = "apple"
+    hangman = Hangman.new()
+    attempts_remaining = 1
+    expect(hangman.guess_word(attempts_remaining, word, letter)).to eq(0)
+  end
+
+  it "given correct letter 'a', it updates letter in computer word 'apple'" do
+    computer_word = "apple"
+    guessed_word = "_____"
+    correct_letter = "a"
+    hangman = Hangman.new()
+    expect(hangman.show_letter(computer_word, guessed_word, correct_letter)).to eq("a____")
+  end
+
+  it "given correct letter 'e', it updates letter in word 'apple'" do
+    computer_word = "apple"
+    guessed_word = "a____"
+    correct_letter = "e"
+    hangman = Hangman.new()
+    expect(hangman.show_letter(computer_word, guessed_word, correct_letter)).to eq("a___e")
+  end
+
+  it "given correct letter 'p', it updates letter in word 'apple'" do
+    computer_word = "apple"
+    guessed_word = "a____"
+    correct_letter = "p"
+    hangman = Hangman.new()
+    expect(hangman.show_letter(computer_word, guessed_word, correct_letter)).to eq("app__")
+  end
+
+  it "given correct letter 'a', it updates letter in word 'banana'" do
+    computer_word = "banana"
+    guessed_word = "b_____"
+    correct_letter = "a"
+    hangman = Hangman.new()
+    expect(hangman.show_letter(computer_word, guessed_word, correct_letter)).to eq("ba_a_a")
+  end
+end
+
+describe WordFileReader do
+  it "returns list of words" do
+    word_file_reader = WordFileReader.new()
+    result = word_file_reader.read_file(File.expand_path("test_words.txt")) # needed to generate an absolute file path for the text file
+    expect(result).to eq(["Apple", "Banana", "Mango", "Melon", "Pineapple"])
+  end
+
+  it "returns list of words" do
+    word_file_reader = WordFileReader.new()
+    result = word_file_reader.read_file(File.expand_path("test_words.txt"))
+    expect(result).not_to eq(["Apple","Mango", "Melon", "Pineapple"])
+    end
+end
